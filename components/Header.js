e1fbb10e4ead7fb721f7d62b84a1dc442df3b09c
BIBruington <psychological_chemist@hotmail.com>
2023-01-23 00:19:10 -0500

    header ui

--- components/Header.js
+++ components/Header.js
@@ -17,21 +17,27 @@ import {
 function Header() {
 
   return (
-    <div className="flex items-center">
-      <h1>header</h1>
+    <div className="m-3">
+
       {/* left */}
-      <div>
+
+      <div className="flex items-center">
         <Image src="https://links.papareact.com/5me"
         width={40} height={40} 
         alt=''
         />
-        <div className="flex">
-          <SearchIcon className="h-6"/>
-          <input type="text" placeholder="Search Facebook" />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 mr-2 text-gray-500"/>
+          <input className="flex items-center bg-transparent outline-none placeholder-gray-500" 
+          type="text" placeholder="Search Facebook" />
         </div>
       </div>
 
       {/* middle */}
+
+      <div>
+        
+      </div>
       
       {/* right */}
     
