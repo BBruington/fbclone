611948a02adfefa67c404413f65a71398462ae4e
BIBruington <psychological_chemist@hotmail.com>
2023-02-04 20:15:45 -0500

    icons input feed

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,8 +1,15 @@
 import Image from "next/legacy/image"
+import { EmojiHappyIcon } from "@heroicons/react/solid";
+import { CameraIcon, VideoCameraIcon } from "@heroicons/react/outline";
+
 export default function InputBox({fbuser}) {
 
+  const sendPost = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
+    <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
       <div className="flex space-x-4 p-4 items-center">
         <Image 
           className="rounded-full"
@@ -17,10 +24,28 @@ export default function InputBox({fbuser}) {
           <input 
             className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
             type="text" 
-            placeholder={`what's on your mind, ${fbuser.user.name}`} 
+            placeholder={`what's on your mind, ${fbuser.user.name}?`} 
           />
+
+          <button hidden type='submit' onClick={sendPost}>Submit</button>
         </form>
       </div>
+
+      <div>
+        <div>
+          <VideoCameraIcon className="h-7 text-red-500"/>
+          <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
+        </div>
+        <div>
+        <CameraIcon className="h-7 text-green-400" />
+          <p className="text-xs sm:text-sm xl:text-base">Phote/Video</p>
+        </div>
+        <div>
+          <EmojiHappyIcon className="h-7 text-yellow-300" />
+          <p className="text-xs sm:text-sm xl:text-base">Feeling/Activity</p>
+        </div>
+      </div>
+
     </div>
   )
 }
